fa5daedf25eeef5332e2f2b350fd33faff072b4b
Doris Duaka <dorisduaka@gmail.com>
2024-11-18 17:17:53 +0000

    Added the icon url to show current icons

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ function refreshWeather(response) {
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
-  
-cityElement.innerHTML = response.data.city;
+let date = new Date(response.data.time * 1000); 
+ let iconElement = document.querySelector("#icon");
 
-
-   timeElement.innerHTML = formatDate(date);
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+ cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
