da9db6c0c38b24b8aa0610f85d3b06cb0ad4d3ad
Doris Duaka <dorisduaka@gmail.com>
2024-11-18 18:11:45 +0000

    updated the api url

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ function refreshWeather(response) {
   let iconElement = document.querySelector("#icon");
 
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" alt="Weather Icon" />`;
-cityElement.innerHTML = response.data.city;
+  cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  temperatureElement.innerHTML = Math.round(temperature);
+  temperatureElement.innerHTML = Math.round(temperature); 
 }
 
 function formatDate(date) {
@@ -39,10 +39,11 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apikey = "42t6213cf48c3o5336a35503b83be79d";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
+  let apikey = "42t6213cf48c3o5336a35503b83be79d"; // Your API key
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`; // Construct API URL with city, API key, and units
+  axios.get(apiUrl).then(refreshWeather); // Send GET request to the API
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
