a6e7f23ff266e2dbc17392b025a59a8c84fb58fc
Doris Duaka <dorisduaka@gmail.com>
2024-12-01 22:54:51 +0000

    updated html and injected the forecast HTML from Javascript

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function refreshWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  temperatureElement.innerHTML = Math.round(temperature); 
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function formatDate(date) {
@@ -50,7 +50,30 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+         <div class="weather-forecast-icon">⛅</div> 
+         <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+          <strong>15°</strong>
+          </div>
+          <div class="weather-forecast-temperature">9°</div>
+        </div> 
+        </div>
+        `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Paris");
+displayForecast();
